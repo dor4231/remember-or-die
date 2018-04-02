e84866980a7957f4232380394b7780c6b5fbef5c
Dor Bar <dor4231@gmail.com>
2018-04-02 22:02:26 +0300

    refactor: Add comments and change code style
    Code readability improvements. Add challenge mode.

--- js/app.js
+++ js/app.js
@@ -13,17 +13,22 @@ document.addEventListener("DOMContentLoaded", function () {
     ];
     const imgStackTemp = [];
 
+    // Declaring global variables to change later
     let player;
     let fullHealth;
     let firstCard;
     let secondCard;
     let cardBoard;
+    let challenge = false;
     let waiting = true;
 
+    // Waiting the number of "ms" (milliseconds) passed as an argument
     function sleep(ms) {
         return new Promise(resolve => setTimeout(resolve, ms));
     }
 
+    // Creating an array of number pairs from 1 to "number"/2
+    // For example: createDeck(4) => [1,1,2,2]
     function createDeck(number) {
         console.log(`Creating ${number} cards!`);
         const deck = new Array(number);
@@ -34,9 +39,10 @@ document.addEventListener("DOMContentLoaded", function () {
         return deck;
     }
 
-
+    // Changing the player state depending on the wrong guess he make
+    // If user choose "Challenging Mode" the he can lose the game and die
     const changePlayerState = function () {
-        if (player.health <= 0) {
+        if (player.health <= 0 && challenge) {
             playerState.src = "assets/images/Dead Face.png";
             playerState.style.backgroundColor = "#999";
             endGame("lose");
@@ -52,6 +58,8 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     };
 
+    // Gets an array and return a shuffled one
+    // That's how I generate different card board every time.
     const shuffleDeck = function (array) {
         console.log(`Shuffling ${array.length} cards!`);
         let currentIndex = array.length, temporaryValue, randomIndex;
@@ -71,17 +79,22 @@ document.addEventListener("DOMContentLoaded", function () {
         return array;
     };
 
-    const createCards = function (number) {
-        console.log(`Creating ${number} Cards!`);
-        for (let i = 0; i < number; i++) {
+    // Creating and appending the cards into th card-board (the document).
+    const createCards = function (deck) {
+        for (let i = 0; i < deck.length; i++) {
             const div = document.createElement("div");
             div.classList.add("play-card");
-            div.id = `${i}`; // cardBoard[i]
+            div.id = `${i}`;
 
             board.appendChild(div)
         }
     };
 
+    // Creating and HTTP GET request to get the images from the array "imgStack"
+    // I save the reposed images as binaries blob to save them in memory.
+    // That way we avoid the multiple GET requests every the user flips a card.
+    // Other reason is that the images may change from time to time and can be
+    // change during the game.
     const downloadImages = function () {
         for (const img of imgStack) {
             const oReq = new XMLHttpRequest();
@@ -96,13 +109,7 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     };
 
-    const cardBoardGenerator = function (size) {
-        let deck = createDeck(size);
-        cardBoard = shuffleDeck(deck);
-        createCards(size);
-        console.log(`Board Generated`)
-    };
-
+    // Flips the card
     const flipCard = function (card) {
         if (card.className.includes("selected") &&
             !player.foundCards.includes((card))) {
@@ -117,10 +124,35 @@ document.addEventListener("DOMContentLoaded", function () {
             card.style.backgroundSize = "cover";
             card.style.backgroundPosition = "center";
         }
-
     };
 
+    // Compare the two cards if matched add cards to foundCards
+    // if not matched flip cards over and lose life. Cards will stay visible
+    // For one second. While card are visible user can't flip other cards.
+    const compereCards = async function() {
+        if (cardBoard[secondCard.id] === cardBoard[firstCard.id]) {
+            firstCard.classList.add("matched");
+            secondCard.classList.add("matched");
+            player.foundCards.push(firstCard, secondCard);
+            if (player.foundCards.length >= cardBoard.length) {
+                endGame("win");
+            }
+        } else {
+            player.health -= 1;
+            changePlayerState();
+            waiting = false;
+            await sleep(1000);
+            waiting = true;
+            flipCard(firstCard);
+            flipCard(secondCard);
+        }
+        player.moves += 1;
+        changePlayerState();
+        document.querySelector(".moves span").innerHTML = player.moves;
+        [firstCard, secondCard] = [null, null]
+    };
 
+    // Clearing the interval
     const endGame = function (type) {
         clearInterval(player.time);
         for (const card of document.querySelectorAll(".card-board .play-card")) {
@@ -131,29 +163,34 @@ document.addEventListener("DOMContentLoaded", function () {
         document.querySelector(`.${type}`).classList.remove("hidden");
     };
 
+    const cardBoardGenerator = function(size) {
+        // Create the cards deck, shuffle it and pu into the global "cardBoard"
+        // variable.
+        cardBoard = shuffleDeck(createDeck(size));
+        // Creating cards as the length of card board.
+        createCards(cardBoard);
+    };
+
 
     //////////////////
     // Timer
     /////////////////
 
-    const startTimer = function () {
+    const startTimer = function(losingHealth) {
         const timer = document.querySelector(".timer span");
         let timerSeconds = 0;
         return setInterval(async function () {
             timerSeconds += 1;
             let M = Math.floor(timerSeconds / 60);
             let S = timerSeconds % 60;
-            if (S < 10 && M < 10) {
-                timer.innerText = `0${M}:0${S}`
-            } else if (S < 10) {
-                timer.innerText = `${M}:0${S}`
-            } else if (M < 10) {
-                timer.innerText = `0${M}:${S}`
-            } else {
-                timer.innerText = `${M}:${S}`
-            }
 
-            if (S % 15 === 0) {
+            // Formatting the timer to be like: 01:05 instead of 1:5
+            if (S < 10) S = `0${S}`;
+            if (M < 10) M = `0${M}`;
+            timer.innerText = `${M}:${S}`;
+
+            // Player losing health every "losingHealth" seconds
+            if (S % losingHealth === 0) {
                 for (let i = 0; i <= 4; i++) {
                     timer.style.color = "#1a1a1a";
                     await sleep(200);
@@ -170,37 +207,20 @@ document.addEventListener("DOMContentLoaded", function () {
     // Event Listeners
     //
 
-    board.addEventListener("click", async function (event) {
-        if (event.target.tagName === "DIV" &&
-            event.target.className.includes("play-card") &&
-            waiting) {
-            const card = event.target;
+    board.addEventListener("click", function (event) {
+        const card = event.target;
+        if (card.className.includes("play-card")
+            && waiting
+            && card !== firstCard
+            && card !== secondCard){
             flipCard(card);
             if (firstCard == null) {
                 firstCard = card
-            } else {
+            } else if (secondCard == null) {
                 secondCard = card;
-                if (cardBoard[secondCard.id] === cardBoard[firstCard.id]) {
-                    console.log("You found a match!");
-                    firstCard.classList.add("matched");
-                    secondCard.classList.add("matched");
-                    player.foundCards.push(firstCard, secondCard);
-                    if (player.foundCards.length >= 16) {
-                        endGame("win");
-                    }
-                } else {
-                    player.health -= 1;
-                    changePlayerState();
-                    waiting = false;
-                    await sleep(1000);
-                    waiting = true;
-                    flipCard(firstCard);
-                    flipCard(secondCard);
-                }
-                player.moves += 1;
-                changePlayerState();
-                document.querySelector(".moves span").innerHTML = player.moves;
-                [firstCard, secondCard] = [null, null]
+                compereCards();
+            } else{
+                console.log("Too Many Clicks!");
             }
         }
     });
@@ -256,7 +276,7 @@ document.addEventListener("DOMContentLoaded", function () {
         fullHealth = player.health;
         document.querySelector("#usernameDisplay").innerHTML = player.name;
         cardBoardGenerator(16);
-        player.time = startTimer();
+        player.time = startTimer(player.losingHealth);
     }
 
     downloadImages();
--- js/player.js
+++ js/player.js
@@ -1,9 +1,10 @@
 class Player {
     constructor(name) {
         this.name = name;
-        this.health = 13;
+        this.health = 15;
         this.moves = 0;
         this.time = 0;
         this.foundCards = [];
+        this.losingHealth = 15;
     }
 }
